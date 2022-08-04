395fbff280c24e9e0d7e100de3366c60ea8c601e
Tiago Santos <tiago881@gmail.com>
2022-08-04 17:37:11 -0300

    feat:add Navbar component

--- movies-lib/src/App.jsx
+++ movies-lib/src/App.jsx
@@ -1,15 +1,11 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
+
+import { Navbar } from './components/Navbar';
 
 function App() {
   return (
     <div>
-      <nav id='navbar'>
-        <h2>
-          <Link to='/'>Movies</Link>
-        </h2>
-        <Link to='/movie/1'>Movie-1</Link>
-        <Link to='/search'>Search</Link>
-      </nav>
+      <Navbar />
       <Outlet />
     </div>
   );
